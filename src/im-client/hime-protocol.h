c1036c5dafbbb300daa9ec0eca48715641229f55
letoh <letoh.tw@gmail.com>
2012-02-20 00:43:40 +0800

    pick b7f6cc4: Fix a segfault bug. the size of sock_path should be UNIX_PATH_MAX.

--- src/im-client/hime-protocol.h
+++ src/im-client/hime-protocol.h
@@ -96,7 +96,7 @@ typedef struct {
 } Server_IP_port;
 
 typedef struct {
-  char sock_path[80];
+  char sock_path[UNIX_PATH_MAX];
 } Server_sock_path;
 #if UNIX
 void __hime_enc_mem(u_char *p, int n, HIME_PASSWD *passwd, u_int *seed);
