80973ea51fdc331c969ef827c5a41b65af3473f8
letoh <letoh.tw@gmail.com>
2012-02-19 18:30:38 +0800

    pick d31c87c: Add git commit hash to about

--- src/about.c
+++ src/about.c
@@ -77,7 +77,11 @@ void create_about_window()
     image = gtk_image_new_from_file (hime_png);
 #endif
 
-    label_version = gtk_label_new ("version " HIME_VERSION "  " __DATE__);
+#if GIT_HAVE
+    label_version = gtk_label_new ("version " HIME_VERSION "\n(git " GIT_HASH ")");
+#else
+    label_version = gtk_label_new ("version " HIME_VERSION);
+#endif
 
     gtk_box_pack_start (GTK_BOX (hbox), image, FALSE, FALSE, 3);
     gtk_box_pack_start (GTK_BOX (hbox), label_version, FALSE, FALSE, 3);
